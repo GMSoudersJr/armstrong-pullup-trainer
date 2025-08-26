049a2d11e7799a20b7b6bcc8c67a63cc7753ccdd
Gerald M. Souders Jr <gmsoudersjr@gmail.com>
2025-08-26 22:35:06 +0900

    fix: get previous data for day 5 buttons

--- src/lib/components/day-five/HardestDaySelector.svelte
+++ src/lib/components/day-five/HardestDaySelector.svelte
@@ -1,31 +1,50 @@
 <script lang="ts">
-	import { initialWorkoutData } from '$lib/initialWorkoutData';
+	import {
+		getCurrentWeekNumber,
+		getWorkoutsbyWeekNumber
+	} from '$lib/indexedDB/actions';
 	import type { ArmstrongDayNumber } from '$lib/types';
+	import type { TDayComplete } from '$lib/indexedDB/definitions';
 	import HardestDayButton from './HardestDayButton.svelte';
 
 	interface Props {
 		selectedDay?: ArmstrongDayNumber;
 	}
 
-	const previousWorkouts = initialWorkoutData.slice(0, -1);
+	let previousWorkouts = $state<TDayComplete[]>([]);
 
 	let { selectedDay = $bindable() }: Props = $props();
+
+	$effect(() => {
+		const getPreviousWorkouts = async () => {
+			const currentWeekNumber = await getCurrentWeekNumber();
+			if (currentWeekNumber > 0) {
+				const workouts = await getWorkoutsbyWeekNumber(currentWeekNumber);
+				previousWorkouts = workouts.filter((w) => w.dayNumber < 5);
+			}
+		};
+		getPreviousWorkouts();
+	});
 </script>
 
 <div class="heading-wrapper">
 	<h3 class="day-control-heading">Select your hardest day!</h3>
 </div>
-<ul class="hardest-day-list">
-	{#each previousWorkouts as workoutDay (workoutDay.workoutData.day)}
-		<li>
-			<HardestDayButton
-				dayNumber={workoutDay.workoutData.day}
-				dayName={workoutDay.workoutData.name}
-				bind:selectedDay
-			/>
-		</li>
-	{/each}
-</ul>
+{#if previousWorkouts.length > 0}
+	<ul class="hardest-day-list">
+		{#each previousWorkouts as workoutDay (workoutDay.dayNumber)}
+			<li>
+				<HardestDayButton
+					dayNumber={workoutDay.dayNumber}
+					dayName={workoutDay.dayAbbreviation}
+					bind:selectedDay
+				/>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No previous workouts found for this week.</p>
+{/if}
 
 <style>
 	.heading-wrapper {
